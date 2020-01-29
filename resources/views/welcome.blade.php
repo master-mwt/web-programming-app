feebbcd9326de4f90df0dd07b0842646d7b9d639
valentinodg <valentino.digiosaffatte@student.univaq.it>
2020-01-29 15:48:56 +0100

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container"></div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row justify-content-center">
+                <h1>welcome page</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
