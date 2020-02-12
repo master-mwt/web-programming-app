9a159221cdd19de16abcc509065b7913e8ee4ddc
ricdip <riccardodpr@gmail.com>
2020-02-12 17:34:58 +0100

    ServiceSeeder

--- database/seeds/ServiceSeeder.php
+++ database/seeds/ServiceSeeder.php
@@ -11,6 +11,30 @@ class ServiceSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $services = [
+            ['name' => 'create_post'],
+            ['name' => 'delete_post'],
+
+            ['name' => 'create_comment'],
+            ['name' => 'delete_comment'],
+
+            ['name' => 'create_channel'],
+            ['name' => 'delete_channel'],
+            ['name' => 'modify_channel_data'],
+
+            ['name' => 'ban_user_from_channel'],    // soft ban
+            ['name' => 'ban_user_from_platform'],   // hard ban
+
+            ['name' => 'create_user'],
+            ['name' => 'delete_user'],
+            ['name' => 'modify_user_data'],
+
+            ['name' => 'access_to_log'],
+            ['name' => 'access_to_backend'],
+        ];
+
+        foreach($services as $service) {
+            App\Service::create($service);
+        }
     }
 }
