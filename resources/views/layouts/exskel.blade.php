95e254cbf0bbdbb81a1ce29a114ae1a664ea7481
valentinodg <valentino.digiosaffatte@student.univaq.it>
2020-03-01 21:49:41 +0100

    res fix

--- resources/views/layouts/exskel.blade.php
+++ resources/views/layouts/exskel.blade.php
@@ -87,7 +87,7 @@ to get the desired effect
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="dist/imgs/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <img src="./imgs/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   John Pierce
