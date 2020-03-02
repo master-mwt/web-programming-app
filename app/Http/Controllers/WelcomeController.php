8dae699dd2e2926ab89c819064d7189d6cd3e1ac
valentinodg <valentino.digiosaffatte@student.univaq.it>
2020-03-03 00:17:52 +0100

    update posts ui and welcome controller

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Channel;
+use App\User;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
@@ -16,6 +18,11 @@ class WelcomeController extends Controller
     {
         //remember to call a complex query from (for example) the PostController that returns the post meta-object, with all the references (for example: User, Channel) already resolved
         $posts = Post::paginate(10);
+
+        foreach($posts as $post) {
+            $post->channel_id = Channel::findOrFail($post->channel_id);
+            $post->user_id = User::findOrFail($post->user_id);
+        }
 
         return view('welcome', compact(
         'posts',
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,19 +6,33 @@
     <div class="row justify-content-center">
         <div class="col-md-12 text-center infinite-scroll">
         @foreach($posts as $post)
-            <div class="card col-lg-7 mx-auto d-flex flex-row px-0 pr-3">
-                <div class="bg-dark rounded-left mr-2 py-3 d-flex flex-column" style="flex: 0 0 50px">
-                    <a href=""><i class="fas fa-arrow-up mb-1"></i></a>
-                    <!-- <span class="mb-1">{{ $post->id}}</span> -->
-                    <a href=""><i class="fas fa-arrow-down"></i></a>
+            <div class="card col-lg-10 mx-auto d-flex flex-row px-0">
+                <div class="bg-dark rounded-left py-3 d-flex flex-column" style="flex: 0 0 50px">
+                    <a href="" class=""><i class="fas fa-arrow-up mb-1"></i></a>
+                    <span class="my-1">{{ $post->id }}</span>
+                    <a href="" class=""><i class="fas fa-arrow-down"></i></a>
                 </div>
                 <div>
-                    <div class="card-header text-left bg-transparent border-0 px-0">
-                        <p class="m-0 mb-1"><b>{{ $post->channel_id }}</b> <span class="text-muted">Posted by {{ $post->user_id }}</span> </p>
+                    <div class="card-header text-left bg-transparent border-0 px-3">
+                        <p class="m-0 mb-1"><b>{{ $post->channel_id->name }} &#183</b> <span class="text-muted">Posted by </span><span class="text-primary text-bold">{{ $post->user_id->name }}</span></p>
                         <h5 class="m-0">{{ $post->title }}</h5>
                     </div>
-                    <div class="card-body text-left px-0 py-1">
+                    <div class="card-body text-left px-3 py-1">
                         <p>{{ $post->content }}</p>
+                    </div>
+                    <div class="card-footer border-0 p-1 px-3 text-left" style="border-bottom-left-radius: 0px">
+                        <a href="" class="text-decoration-none mr-2"><i class="fas fa-comment-alt mr-1"></i>100 Comments</a>
+                        <a href="" class="text-decoration-none mr-2"><i class="fas fa-crown mr-1"></i>Give Award</a>
+                        <a href="" class="text-decoration-none mr-2"><i class="fas fa-bookmark mr-1"></i>Save</a>
+                        <a href="" class="text-decoration-none mr-2"><i class="fas fa-ban mr-1"></i>Hide</a>
+                        <a href="" class="text-decoration-none "><i class="fas fa-flag mr-1"></i>Report</a>
+                        
+                        <!-- buttons -->
+                        <!-- <button type="button" class="btn btn-sm btn-outline-dark"><i class="fas fa-comment-alt mr-1"></i>100 Comments</button>
+                        <button type="button" class="btn btn-sm btn-outline-dark"><i class="fas fa-crown mr-1"></i>Give Award</button>
+                        <button type="button" class="btn btn-sm btn-outline-dark"><i class="fas fa-bookmark mr-1"></i>Save</button>
+                        <button type="button" class="btn btn-sm btn-outline-dark"><i class="fas fa-ban mr-1"></i>Hide</button>
+                        <button type="button" class="btn btn-sm btn-outline-dark"><i class="fas fa-flag mr-1"></i>Report</button> -->
                     </div>
                 </div>
             </div>
