744fcfcf7b1e78627e7c86e9cd63add79af4335f
ricdip <riccardodpr@gmail.com>
2020-03-30 18:34:54 +0200

    CommentFactory fix

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -7,14 +7,14 @@ use Faker\Generator as Faker;
 use \DavidBadura\FakerMarkdownGenerator\FakerProvider;
 
 $factory->define(Comment::class, function (Faker $faker) {
-    $faker->addProvider(new FakerProvider($faker));
+    // $faker->addProvider(new FakerProvider($faker));
 
     $user = \App\User::all()->random(1)->first();
     $reply = \App\Reply::all()->random(1)->first();
 
     return [
         'title' => $faker->sentence,
-        'content' => $faker->text(),
+        'content' => $faker->text,
         'upvote' => $faker->numberBetween(0,50),
         'downvote' => $faker->numberBetween(0,50),
         // FKs
