78c87e2085b78a6a2fbe508e5965133c9ac713dd
ricdip <riccardodpr@gmail.com>
2020-04-12 19:31:02 +0200

    Permission seeding

--- database/seeds/GroupServiceSeeder.php
+++ database/seeds/GroupServiceSeeder.php
@@ -11,39 +11,80 @@ class GroupServiceSeeder extends Seeder
      */
     public function run()
     {
+        // GROUPS
+        $administrator = \App\Group::where('name', 'administrator')->first()->id;
+        $logged = \App\Group::where('name', 'logged')->first()->id;
+
+        // SERVICES
+        $create_post = \App\Service::where('name', 'create_post')->first()->id;
+        $delete_post = \App\Service::where('name', 'delete_post')->first()->id;
+        $create_reply = \App\Service::where('name', 'create_reply')->first()->id;
+        $delete_reply = \App\Service::where('name', 'delete_reply')->first()->id;
+        $create_comment = \App\Service::where('name', 'create_comment')->first()->id;
+        $delete_comment = \App\Service::where('name', 'delete_comment')->first()->id;
+        $create_channel = \App\Service::where('name', 'create_channel')->first()->id;
+        $delete_channel = \App\Service::where('name', 'delete_channel')->first()->id;
+        $mod_channel_data = \App\Service::where('name', 'mod_channel_data')->first()->id;
+        $ban_user_from_channel = \App\Service::where('name', 'ban_user_from_channel')->first()->id;
+        $ban_user_from_platform = \App\Service::where('name', 'ban_user_from_platform')->first()->id;
+        $create_user = \App\Service::where('name', 'create_user')->first()->id;
+        $delete_user = \App\Service::where('name', 'delete_user')->first()->id;
+        $mod_user_data = \App\Service::where('name', 'mod_user_data')->first()->id;
+        $view_user_data = \App\Service::where('name', 'view_user_data')->first()->id;
+        $access_to_log = \App\Service::where('name', 'access_to_log')->first()->id;
+        $access_to_backend = \App\Service::where('name', 'access_to_backend')->first()->id;
+        $silence_user_in_comment_section = \App\Service::where('name', 'silence_user_in_comment_section')->first()->id;
+        $report_user_in_channel = \App\Service::where('name', 'report_user_in_channel')->first()->id;
+        $upgrade_to_moderator = \App\Service::where('name', 'upgrade_to_moderator')->first()->id;
+        $upgrade_to_admin = \App\Service::where('name', 'upgrade_to_admin')->first()->id;
+        $downgrade_moderator = \App\Service::where('name', 'downgrade_moderator')->first()->id;
+        $downgrade_admin = \App\Service::where('name', 'downgrade_admin')->first()->id;
+        $report_post = \App\Service::where('name', 'report_post')->first()->id;
+        $view_channel_members_list = \App\Service::where('name', 'view_channel_members_list')->first()->id;
+
+        // BINDING GROUP - SERVICE
         $groupsServices = [
-            // administrator: group id 1
-            ['group_id' => 1, 'service_id' => 1],   //create_post
-            ['group_id' => 1, 'service_id' => 2],   //delete_post
+            // administrator
+            ['group_id' => $administrator, 'service_id' => $create_post],   //create_post
+            ['group_id' => $administrator, 'service_id' => $delete_post],   //delete_post
 
-            ['group_id' => 1, 'service_id' => 3],   //create_post
-            ['group_id' => 1, 'service_id' => 4],   //delete_post
+            ['group_id' => $administrator, 'service_id' => $create_reply],   //create_post
+            ['group_id' => $administrator, 'service_id' => $delete_reply],   //delete_post
 
-            ['group_id' => 1, 'service_id' => 5],   //create_comment
-            ['group_id' => 1, 'service_id' => 6],   //delete_comment
+            ['group_id' => $administrator, 'service_id' => $create_comment],   //create_comment
+            ['group_id' => $administrator, 'service_id' => $delete_comment],   //delete_comment
 
-            ['group_id' => 1, 'service_id' => 7],   //create_channel
-            ['group_id' => 1, 'service_id' => 8],   //delete_channel
-            ['group_id' => 1, 'service_id' => 9],   //mod_channel_data
+            ['group_id' => $administrator, 'service_id' => $create_channel],   //create_channel
+            ['group_id' => $administrator, 'service_id' => $delete_channel],   //delete_channel
+            ['group_id' => $administrator, 'service_id' => $mod_channel_data],   //mod_channel_data
 
-            ['group_id' => 1, 'service_id' => 10],   //ban_user_from_channel
-            ['group_id' => 1, 'service_id' => 11],   //ban_user_from_platform
+            ['group_id' => $administrator, 'service_id' => $ban_user_from_channel],   //ban_user_from_channel
+            ['group_id' => $administrator, 'service_id' => $ban_user_from_platform],   //ban_user_from_platform
 
-            ['group_id' => 1, 'service_id' => 12],  //create_user
-            ['group_id' => 1, 'service_id' => 13],  //delete_user
-            ['group_id' => 1, 'service_id' => 14],  //mod_user_data
+            ['group_id' => $administrator, 'service_id' => $create_user],  //create_user
+            ['group_id' => $administrator, 'service_id' => $delete_user],  //delete_user
+            ['group_id' => $administrator, 'service_id' => $mod_user_data],  //mod_user_data
+            ['group_id' => $administrator, 'service_id' => $view_user_data],  //view_user_data
 
-            ['group_id' => 1, 'service_id' => 15],  //access_to_log
-            ['group_id' => 1, 'service_id' => 16],  //access_to_backend
+            ['group_id' => $administrator, 'service_id' => $access_to_log],  //access_to_log
+            ['group_id' => $administrator, 'service_id' => $access_to_backend],  //access_to_backend
 
-            ['group_id' => 1, 'service_id' => 17],  //silence_user_in_comment_section
-            ['group_id' => 1, 'service_id' => 18],  //report_user_in_channel
-            
-            // logged user: group id 2
-            ['group_id' => 2, 'service_id' => 7],   //create_channel
+            ['group_id' => $administrator, 'service_id' => $silence_user_in_comment_section],  //silence_user_in_comment_section
+            ['group_id' => $administrator, 'service_id' => $report_user_in_channel],  //report_user_in_channel
 
-            ['group_id' => 2, 'service_id' => 13],  //delete_user
-            ['group_id' => 2, 'service_id' => 14],  //mod_user_data
+            ['group_id' => $administrator, 'service_id' => $upgrade_to_moderator],  //upgrade_to_moderator
+            ['group_id' => $administrator, 'service_id' => $upgrade_to_admin],  //upgrade_to_admin
+            ['group_id' => $administrator, 'service_id' => $downgrade_moderator],  //downgrade_moderator
+            ['group_id' => $administrator, 'service_id' => $downgrade_admin],  //downgrade_admin
+            ['group_id' => $administrator, 'service_id' => $report_post],  //report_post
+            ['group_id' => $administrator, 'service_id' => $view_channel_members_list],  //view_channel_members_list
+
+            // logged
+            ['group_id' => $logged, 'service_id' => $create_channel],   //create_channel
+
+            ['group_id' => $logged, 'service_id' => $mod_user_data],  //mod_user_data
+            ['group_id' => $logged, 'service_id' => $view_user_data],  //view_user_data
+
         ];
 
         foreach($groupsServices as $groupService) {
--- database/seeds/RoleServiceSeeder.php
+++ database/seeds/RoleServiceSeeder.php
@@ -11,37 +11,111 @@ class RoleServiceSeeder extends Seeder
      */
     public function run()
     {
+        // ROLES
+        $creator = \App\Role::where('name', 'creator')->first()->id;
+        $admin = \App\Role::where('name', 'admin')->first()->id;
+        $moderator = \App\Role::where('name', 'moderator')->first()->id;
+        $member = \App\Role::where('name', 'member')->first()->id;
+
+        // SERVICES
+        $create_post = \App\Service::where('name', 'create_post')->first()->id;
+        $delete_post = \App\Service::where('name', 'delete_post')->first()->id;
+        $create_reply = \App\Service::where('name', 'create_reply')->first()->id;
+        $delete_reply = \App\Service::where('name', 'delete_reply')->first()->id;
+        $create_comment = \App\Service::where('name', 'create_comment')->first()->id;
+        $delete_comment = \App\Service::where('name', 'delete_comment')->first()->id;
+        $delete_channel = \App\Service::where('name', 'delete_channel')->first()->id;
+        $mod_channel_data = \App\Service::where('name', 'mod_channel_data')->first()->id;
+        $ban_user_from_channel = \App\Service::where('name', 'ban_user_from_channel')->first()->id;
+        $silence_user_in_comment_section = \App\Service::where('name', 'silence_user_in_comment_section')->first()->id;
+        $report_user_in_channel = \App\Service::where('name', 'report_user_in_channel')->first()->id;
+        $upgrade_to_moderator = \App\Service::where('name', 'upgrade_to_moderator')->first()->id;
+        $upgrade_to_admin = \App\Service::where('name', 'upgrade_to_admin')->first()->id;
+        $downgrade_moderator = \App\Service::where('name', 'downgrade_moderator')->first()->id;
+        $downgrade_admin = \App\Service::where('name', 'downgrade_admin')->first()->id;
+        $report_post = \App\Service::where('name', 'report_post')->first()->id;
+        $view_channel_members_list = \App\Service::where('name', 'view_channel_members_list')->first()->id;
+
+        // BINDING ROLE - SERVICE
         $rolesServices = [
-            // creator: role id 1
-            ['role_id' => 1, 'service_id' => 1],    //create_post
-            ['role_id' => 1, 'service_id' => 2],    //delete_post
-            ['role_id' => 1, 'service_id' => 3],    //create_comment
-            ['role_id' => 1, 'service_id' => 4],    //delete_comment
-            ['role_id' => 1, 'service_id' => 6],    //delete_channel
-            ['role_id' => 1, 'service_id' => 7],    //mod_channel_data
-            ['role_id' => 1, 'service_id' => 8],    //ban_user_from_channel
-            ['role_id' => 1, 'service_id' => 15],   //silence_user_in_comment_section
-            ['role_id' => 1, 'service_id' => 16],   //report_user_in_channel
-            // admin: role id 2
-            ['role_id' => 2, 'service_id' => 1],    //create_post
-            ['role_id' => 2, 'service_id' => 2],    //delete_post
-            ['role_id' => 2, 'service_id' => 3],    //create_comment
-            ['role_id' => 2, 'service_id' => 4],    //delete_comment
-            ['role_id' => 2, 'service_id' => 8],    //ban_user_from_channel
-            ['role_id' => 2, 'service_id' => 15],   //silence_user_in_comment_section
-            ['role_id' => 2, 'service_id' => 16],   //report_user_in_channel
-            // moderator: role id 3
-            ['role_id' => 3, 'service_id' => 1],    //create_post
-            ['role_id' => 3, 'service_id' => 2],    //delete_post
-            ['role_id' => 3, 'service_id' => 3],    //create_comment
-            ['role_id' => 3, 'service_id' => 4],    //delete_comment
-            ['role_id' => 3, 'service_id' => 15],   //silence_user_in_comment_section
-            ['role_id' => 3, 'service_id' => 16],   //report_user_in_channel
-            // member: role id 4
-            ['role_id' => 4, 'service_id' => 1],    //create_post
-            ['role_id' => 4, 'service_id' => 2],    //delete_post
-            ['role_id' => 4, 'service_id' => 3],    //create_comment
-            ['role_id' => 4, 'service_id' => 4],    //delete_comment
+            // creator
+            ['role_id' => $creator, 'service_id' => $create_post],   //create_post
+            ['role_id' => $creator, 'service_id' => $delete_post],   //delete_post
+
+            ['role_id' => $creator, 'service_id' => $create_reply],   //create_post
+            ['role_id' => $creator, 'service_id' => $delete_reply],   //delete_post
+
+            ['role_id' => $creator, 'service_id' => $create_comment],   //create_comment
+            ['role_id' => $creator, 'service_id' => $delete_comment],   //delete_comment
+
+            ['role_id' => $creator, 'service_id' => $delete_channel],   //delete_channel
+            ['role_id' => $creator, 'service_id' => $mod_channel_data],   //mod_channel_data
+
+            ['role_id' => $creator, 'service_id' => $ban_user_from_channel],   //ban_user_from_channel
+
+            ['role_id' => $creator, 'service_id' => $silence_user_in_comment_section],  //silence_user_in_comment_section
+            ['role_id' => $creator, 'service_id' => $report_user_in_channel],  //report_user_in_channel
+
+            ['role_id' => $creator, 'service_id' => $upgrade_to_moderator],  //upgrade_to_moderator
+            ['role_id' => $creator, 'service_id' => $upgrade_to_admin],  //upgrade_to_admin
+            ['role_id' => $creator, 'service_id' => $downgrade_moderator],  //downgrade_moderator
+            ['role_id' => $creator, 'service_id' => $downgrade_admin],  //downgrade_admin
+            ['role_id' => $creator, 'service_id' => $report_post],  //report_post
+            ['role_id' => $creator, 'service_id' => $view_channel_members_list],  //view_channel_members_list
+
+            // admin
+            ['role_id' => $admin, 'service_id' => $create_post],   //create_post
+            ['role_id' => $admin, 'service_id' => $delete_post],   //delete_post
+
+            ['role_id' => $admin, 'service_id' => $create_reply],   //create_post
+            ['role_id' => $admin, 'service_id' => $delete_reply],   //delete_post
+
+            ['role_id' => $admin, 'service_id' => $create_comment],   //create_comment
+            ['role_id' => $admin, 'service_id' => $delete_comment],   //delete_comment
+
+            ['role_id' => $admin, 'service_id' => $mod_channel_data],   //mod_channel_data
+
+            ['role_id' => $admin, 'service_id' => $ban_user_from_channel],   //ban_user_from_channel
+
+            ['role_id' => $admin, 'service_id' => $silence_user_in_comment_section],  //silence_user_in_comment_section
+            ['role_id' => $admin, 'service_id' => $report_user_in_channel],  //report_user_in_channel
+
+            ['role_id' => $admin, 'service_id' => $upgrade_to_moderator],  //upgrade_to_moderator
+            ['role_id' => $admin, 'service_id' => $downgrade_moderator],  //downgrade_moderator
+            ['role_id' => $admin, 'service_id' => $report_post],  //report_post
+            ['role_id' => $admin, 'service_id' => $view_channel_members_list],  //view_channel_members_list
+
+            // moderator
+            ['role_id' => $moderator, 'service_id' => $create_post],   //create_post
+            ['role_id' => $moderator, 'service_id' => $delete_post],   //delete_post
+
+            ['role_id' => $moderator, 'service_id' => $create_reply],   //create_post
+            ['role_id' => $moderator, 'service_id' => $delete_reply],   //delete_post
+
+            ['role_id' => $moderator, 'service_id' => $create_comment],   //create_comment
+            ['role_id' => $moderator, 'service_id' => $delete_comment],   //delete_comment
+
+            ['role_id' => $moderator, 'service_id' => $ban_user_from_channel],   //ban_user_from_channel
+
+            ['role_id' => $moderator, 'service_id' => $silence_user_in_comment_section],  //silence_user_in_comment_section
+            ['role_id' => $moderator, 'service_id' => $report_user_in_channel],  //report_user_in_channel
+
+            ['role_id' => $moderator, 'service_id' => $report_post],  //report_post
+            ['role_id' => $moderator, 'service_id' => $view_channel_members_list],  //view_channel_members_list
+
+            // member
+            ['role_id' => $member, 'service_id' => $create_post],   //create_post
+            ['role_id' => $member, 'service_id' => $delete_post],   //delete_post
+
+            ['role_id' => $member, 'service_id' => $create_reply],   //create_post
+            ['role_id' => $member, 'service_id' => $delete_reply],   //delete_post
+
+            ['role_id' => $member, 'service_id' => $create_comment],   //create_comment
+            ['role_id' => $member, 'service_id' => $delete_comment],   //delete_comment
+
+            ['role_id' => $member, 'service_id' => $report_post],  //report_post
+            ['role_id' => $member, 'service_id' => $view_channel_members_list],  //view_channel_members_list
+
         ];
 
         foreach($rolesServices as $roleService) {
